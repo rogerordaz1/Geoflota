3ac29e957691f6523ca441d2c659eb656f8b129f
Roger <rogerordaz98@gmail.com>
2021-12-15 15:43:01 -0500

    Revert "ya"
    This reverts commit a2ea912d5f5ffb28a8cf4cca9e05063f911b2297.

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:localizacionversion2/providers/login_form_provider.dart';
+import 'package:localizacionversion2/widgets/widgets.dart';
+import 'package:provider/provider.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
@@ -9,168 +12,49 @@ class LoginPage extends StatefulWidget {
 }
 
 class LoginPageState extends State<LoginPage> {
-  bool _isHidden = true;
+  bool isHidden = true;
   bool controlcmail = false;
   bool controlcontrsena = false;
   final emailcontroller = TextEditingController();
   final passwordcontroller = TextEditingController();
   bool respuestahttp = false;
 
-  void toggleVisibility() {
-    setState(() {
-      _isHidden = !_isHidden;
-    });
-  }
-
-  void dispose() {
-    emailcontroller.dispose();
-    passwordcontroller.dispose();
-    super.dispose();
-  }
-
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
     return Scaffold(
-      backgroundColor: Colors.blue[100],
-      body: Stack(
-        children: [
-          Container(
-            child: logo(),
-            padding: EdgeInsets.only(
-              top: 80,
-              left: MediaQuery.of(context).size.width * 0.35,
-              right: MediaQuery.of(context).size.width * 0.35,
-            ),
-          ),
-          SingleChildScrollView(
-            child: Container(
-              padding: const EdgeInsets.only(top: 125, left: 25, right: 25),
-              child: Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(5),
-                  color: const Color.fromRGBO(255, 255, 255, 0.6),
-                ),
-                padding: const EdgeInsets.only(top: 100, left: 10, right: 10),
-                height: MediaQuery.of(context).size.height * 0.45,
-                width: MediaQuery.of(context).size.width,
+      body: AuthBackground(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              SizedBox(
+                height: size.height * 0.30,
+              ),
+              CardContainer(
                 child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    const Text(
-                      'Correo electrónico',
-                    ),
-                    const SizedBox(
-                      height: 5,
-                    ),
-                    campoemail(),
                     const SizedBox(
                       height: 10,
                     ),
-                    const Text(
-                      'Contraseña',
+                    Text(
+                      'Login ',
+                      style: Theme.of(context).textTheme.headline4,
                     ),
                     const SizedBox(
-                      height: 5,
+                      height: 30,
                     ),
-                    campocontrasena(),
-                    const SizedBox(
-                      height: 10,
-                    ),
-                    MaterialButton(
-                      child: const Text('Continuar'),
-                      minWidth: MediaQuery.of(context).size.width,
-                      height: 40,
-                      onPressed: () {
-                        comprobar(
-                            emailcontroller.text, passwordcontroller.text);
-
-                        // dispose();
-                      },
-                      color: Colors.blue[400],
+                    ChangeNotifierProvider(
+                      create: (_) => LoginFormProvider(),
+                      child: const _LoginForm(),
                     )
                   ],
                 ),
               ),
-            ),
+            ],
           ),
-        ],
+        ),
       ),
     );
-  }
-
-  campoemail() {
-    return Form(
-      autovalidateMode: AutovalidateMode.onUserInteraction,
-      child: TextFormField(
-          controller: emailcontroller,
-          decoration: InputDecoration(
-            contentPadding: const EdgeInsets.fromLTRB(12, 8, 12, 8),
-            border: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(5),
-            ),
-            hintText: "johnDo@gmail.com",
-            hintStyle: const TextStyle(
-              color: Colors.grey,
-              fontSize: 16.0,
-            ),
-            prefixIcon: const Icon(Icons.email),
-          ),
-          validator: (value) {
-            String pattern =
-                r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
-            RegExp regExp = RegExp(pattern);
-
-            if (regExp.hasMatch(value ?? "")) {
-              controlcmail = true;
-              return null;
-            } else {
-              controlcmail = false;
-              return 'Introduzca un correo electronico valido';
-            }
-          }),
-    );
-  }
-
-  campocontrasena() {
-    return Form(
-      autovalidateMode: AutovalidateMode.onUserInteraction,
-      child: TextFormField(
-          controller: passwordcontroller,
-          decoration: InputDecoration(
-              contentPadding: const EdgeInsets.fromLTRB(12, 8, 12, 8),
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(5),
-              ),
-              hintText: "*******",
-              hintStyle: const TextStyle(
-                color: Colors.grey,
-                fontSize: 16.0,
-              ),
-              prefixIcon: const Icon(Icons.lock),
-              suffixIcon: IconButton(
-                onPressed: toggleVisibility,
-                icon: _isHidden
-                    ? const Icon(Icons.visibility_off)
-                    : const Icon(Icons.visibility),
-              )),
-          obscureText: _isHidden,
-          validator: (value) {
-            if ((value != null && value.length > 5)) {
-              controlcontrsena = true;
-              return null;
-            } else {
-              controlcontrsena = false;
-              return "La contrasena debe tener 6 caracteres";
-            }
-          }),
-    );
-  }
-
-  logo() {
-    return const SizedBox(
-        height: 100,
-        width: 100,
-        child: Image(image: AssetImage('assets/foto1.png')));
   }
 
   void comprobar(String email, String contrsena) async {
@@ -189,5 +73,128 @@ class LoginPageState extends State<LoginPage> {
       emailcontroller.clear();
       passwordcontroller.clear();
     }
+  }
+}
+
+class _LoginForm extends StatefulWidget {
+  const _LoginForm({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  State<_LoginForm> createState() => _LoginFormState();
+}
+
+class _LoginFormState extends State<_LoginForm> {
+  bool isHidden = true;
+
+  void toggleVisibility() {
+    setState(() {
+      isHidden = !isHidden;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final loginForm = Provider.of<LoginFormProvider>(context);
+
+    return Form(
+        key: loginForm.formKey,
+        autovalidateMode: AutovalidateMode.onUserInteraction,
+        child: Column(
+          children: [
+            TextFormField(
+              onChanged: (value) => loginForm.email = value,
+              validator: (value) {
+                String pattern =
+                    r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
+                RegExp regExp = RegExp(pattern);
+                return regExp.hasMatch(value ?? '')
+                    ? null
+                    : 'el valor no luce como un correo valido';
+              },
+              keyboardType: TextInputType.emailAddress,
+              decoration: const InputDecoration(
+                  contentPadding:
+                      EdgeInsets.symmetric(horizontal: 8, vertical: 12),
+                  border: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.blue)),
+                  enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.blue)),
+                  focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.blue, width: 2)),
+                  hintText: 'jhon.doe@gmail.com',
+                  labelText: 'Correo',
+                  labelStyle: TextStyle(color: Colors.grey),
+                  prefixIcon: Icon(
+                    Icons.alternate_email_sharp,
+                    color: Colors.blue,
+                  )),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            TextFormField(
+              onChanged: (value) => loginForm.password = value,
+              validator: (value) {
+                if ((value != null && value.length > 5)) {
+                  // controlcontrsena = true;
+                  return null;
+                } else {
+                  // controlcontrsena = false;
+                  return "La contrasena debe tener 6 caracteres";
+                }
+              },
+              keyboardType: TextInputType.emailAddress,
+              decoration: InputDecoration(
+                  contentPadding:
+                      const EdgeInsets.symmetric(horizontal: 8, vertical: 12),
+                  border: const OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.blue)),
+                  enabledBorder: const OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.blue)),
+                  focusedBorder: const OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.blue, width: 2)),
+                  hintText: '******',
+                  labelText: 'Contraseña',
+                  labelStyle: const TextStyle(color: Colors.grey),
+                  prefixIcon: const Icon(
+                    Icons.lock_outline,
+                    color: Colors.blue,
+                  ),
+                  suffixIcon: IconButton(
+                    onPressed: toggleVisibility,
+                    icon: isHidden
+                        ? const Icon(Icons.visibility_off)
+                        : const Icon(Icons.visibility),
+                  )),
+              obscureText: isHidden,
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            MaterialButton(
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10)),
+                disabledColor: Colors.grey,
+                elevation: 0,
+                color: Colors.blue,
+                child: Container(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 80, vertical: 15),
+                    child: const Text(
+                      'Ingresar',
+                      style: TextStyle(color: Colors.white),
+                    )),
+                onPressed: () {
+                  if (!loginForm.isValidForm()) return;
+
+                  Navigator.pushReplacementNamed(context, 'home');
+                }),
+            const SizedBox(
+              height: 20,
+            ),
+          ],
+        ));
   }
 }
